fa345d62599bf77b793f7bf2a48070defcd1a665
monikagupta94159 <monikagupta94159@gmail.com>
2020-09-25 22:54:29 +0530

    routing completed

--- src/app/space-x/space-x.component.ts
+++ src/app/space-x/space-x.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { SpacexService } from './spacex.service';
-import {Router, NavigationExtras} from '@angular/router'
+import {Router, NavigationExtras, ActivatedRoute, Params} from '@angular/router'
 @Component({
   selector: 'app-space-x',
   templateUrl: './space-x.component.html',
@@ -11,20 +11,29 @@ export class SpaceXComponent implements OnInit, OnChanges {
   dateSelected: any;
   launchValue: any;
   landValue: any;
-  selected = false;
 
-  constructor(private spaceXService: SpacexService, private router: Router) { }
+  constructor(private spaceXService: SpacexService, private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params){
+        this.dateSelected = params['launch_year'];
+        this.launchValue = params['launch_success'];
+        this.landValue = params['land_success'];
+      } 
+    })
     let navigationExtras: NavigationExtras = {
       queryParams : {
         'limit': 100,
+        'launch_success': this.launchValue,
+        'land_success': this.landValue,
+        'launch_year': this.dateSelected
       }
     }
     this.router.navigate(['v3/launches'], navigationExtras);
-    this.spaceXService.getSpaceXData().subscribe(data => {
+    this.spaceXService.postDatePlans(this.launchValue, this.landValue, this.dateSelected).subscribe(data => {
       this.spaceXData = data;
-      console.log(' this.spaceXData: ',  this.spaceXData[0].launch_success);
     })
   }
 
@@ -32,7 +41,6 @@ export class SpaceXComponent implements OnInit, OnChanges {
 
   }
   filterDates(date){
-    this.selected = !this.selected;
     this.dateSelected = date;
     let navigationExtras: NavigationExtras = {
       queryParams : {
@@ -44,11 +52,9 @@ export class SpaceXComponent implements OnInit, OnChanges {
     }
     this.spaceXService.postDatePlans(this.launchValue, this.landValue, this.dateSelected).subscribe(data => {
       this.spaceXData = data;
-      console.log('this.spaceXData: datefilters', this.spaceXData);
       this.router.navigate(['v3/launches'], navigationExtras);
   
     })
-    console.log('dateSelected: ', this.dateSelected);
   }
 
    filterLaunch(boolean){
@@ -63,11 +69,8 @@ export class SpaceXComponent implements OnInit, OnChanges {
   }
   this.spaceXService.postDatePlans(this.launchValue, this.landValue, this.dateSelected).subscribe(data => {
     this.spaceXData = data;
-    console.log('this.spaceXData: launch', this.spaceXData);
     this.router.navigate(['v3/launches'], navigationExtras);
-
   })
-    console.log('launchValue: ', this.launchValue);
    }
 
    filterLand(boolean) {
@@ -82,10 +85,8 @@ export class SpaceXComponent implements OnInit, OnChanges {
     }
     this.spaceXService.postDatePlans(this.launchValue, this.landValue, this.dateSelected).subscribe(data => {
       this.spaceXData = data;
-      console.log('this.spaceXData: land', this.spaceXData);
       this.router.navigate(['v3/launches'], navigationExtras);
     })
-    console.log('landValue: ', this.landValue);
    }
 
 }
